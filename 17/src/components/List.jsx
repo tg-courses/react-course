ff7f8f9a7462728b5461802493b4030a330e2b3c
Andreea Bardasu <andreea.bardasu@toroguapo.com>
2025-01-08 15:15:45 +0200

    17

---
+++ 17/src/components/List.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+
+export default function List({ songs, onDelete }) {
+    const listItems = songs.map((song) => {
+        return (
+            <li className="item item-normal">
+                <div className="item-position-buttons item-position-buttons-enabled">
+                    {/* Up button */}
+                    <button className="item-position-button">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            className="icon"
+                        >
+                            <path
+                                fillRule="evenodd"
+                                d="M10 17a.75.75 0 01-.75-.75V5.612L5.29 9.77a.75.75 0 01-1.08-1.04l5.25-5.5a.75.75 0 011.08 0l5.25 5.5a.75.75 0 11-1.08 1.04l-3.96-4.158V16.25A.75.75 0 0110 17z"
+                                clipRule="evenodd"
+                            />
+                        </svg>
+                    </button>
+                    {/* Down button */}
+                    <button className="item-position-button">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            className="icon"
+                        >
+                            <path
+                                fillRule="evenodd"
+                                d="M10 3a.75.75 0 01.75.75v10.638l3.96-4.158a.75.75 0 111.08 1.04l-5.25 5.5a.75.75 0 01-1.08 0l-5.25-5.5a.75.75 0 111.08-1.04l3.96 4.158V3.75A.75.75 0 0110 3z"
+                                clipRule="evenodd"
+                            />
+                        </svg>
+                    </button>
+                </div>
+                <div className="item-title">{song.title}</div>
+                <div className="item-actions">
+                    {/* Delete button */}
+                    {onDelete && (
+                        <button
+                            className="button item-action-button"
+                            onClick={() => onDelete(song)}
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                                className="icon"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M8.75 1A2.75 2.75 0 006 3.75v.443c-.795.077-1.584.176-2.365.298a.75.75 0 10.23 1.482l.149-.022.841 10.518A2.75 2.75 0 007.596 19h4.807a2.75 2.75 0 002.742-2.53l.841-10.52.149.023a.75.75 0 00.23-1.482A41.03 41.03 0 0014 4.193V3.75A2.75 2.75 0 0011.25 1h-2.5zM10 4c.84 0 1.673.025 2.5.075V3.75c0-.69-.56-1.25-1.25-1.25h-2.5c-.69 0-1.25.56-1.25 1.25v.325C8.327 4.025 9.16 4 10 4zM8.58 7.72a.75.75 0 00-1.5.06l.3 7.5a.75.75 0 101.5-.06l-.3-7.5zm4.34.06a.75.75 0 10-1.5-.06l-.3 7.5a.75.75 0 101.5.06l.3-7.5z"
+                                    clipRule="evenodd"
+                                />
+                            </svg>
+                        </button>
+                    )}
+                    {/* Edit button */}
+                    <button className="button item-action-button">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                            className="icon"
+                        >
+                            <path d="M5.433 13.917l1.262-3.155A4 4 0 017.58 9.42l6.92-6.918a2.121 2.121 0 013 3l-6.92 6.918c-.383.383-.84.685-1.343.886l-3.154 1.262a.5.5 0 01-.65-.65z" />
+                            <path d="M3.5 5.75c0-.69.56-1.25 1.25-1.25H10A.75.75 0 0010 3H4.75A2.75 2.75 0 002 5.75v9.5A2.75 2.75 0 004.75 18h9.5A2.75 2.75 0 0017 15.25V10a.75.75 0 00-1.5 0v5.25c0 .69-.56 1.25-1.25 1.25h-9.5c-.69 0-1.25-.56-1.25-1.25v-9.5z" />
+                        </svg>
+                    </button>
+                </div>
+            </li>
+        );
+    });
+
+    return <ul className="content item-list focus">{...listItems}</ul>;
+}
