281d086644b73edf5c2ff943c7a3eaee5492966a
Andreea Bardasu <andreea.bardasu@toroguapo.com>
2025-01-08 14:51:43 +0200

    16

---
+++ 16/src/components/SearchCount.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SearchCount({ filteredCount, totalCount }) {
+    return React.createElement(
+        "div",
+        { className: "header-summary" },
+        `${filteredCount} of ${totalCount} songs`
+    );
+}
