d55e94acbf8f9d2cec2509f55fd10848386ed44b
Andreea Bardasu <andreea.bardasu@toroguapo.com>
2025-05-18 13:35:28 +0300

    React Course

--- exercises/01/01-create-button/src/components/SearchCount.jsx
+++ exercises/01/01-create-button/src/components/SearchCount.jsx
@@ -2,13 +2,15 @@ import React from "react";
 
 import ArrowsUpDownIcon from "./icons/ArrowsUpDownIcon";
 
-export default function SearchCount({ filteredCount, totalCount }) {
+export default function SearchCount({ filteredCount, totalCount, onSort }) {
     return (
         <div className="header-summary">
             <div>{`${filteredCount} of ${totalCount} songs`}</div>
-            <button className="button">
-                <ArrowsUpDownIcon />
-            </button>
+            {onSort && (
+                <button className="button" onClick={onSort}>
+                    <ArrowsUpDownIcon />
+                </button>
+            )}
         </div>
     );
 }
